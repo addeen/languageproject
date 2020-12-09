56dd14e096e77412925872d038692dfccdac2688
addeen <61187259+addeen@users.noreply.github.com>
2020-12-09 14:31:34 +0000

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,263 @@
+var beep1= $("#spainaudio")[0];
+$("#Spain")
+.mouseenter(function(){
+    beep1.play();
+});
+
+var beep2= $("#portugalaudio")[0];
+$("#Portugal")
+.mouseenter(function(){
+    beep2.play();
+});
+
+var beep3= $("#franceaudio")[0];
+$("#France")
+.mouseenter(function(){
+    beep3.play();
+});
+
+var beep4= $("#switzerlandaudio")[0];
+$("#Switzerland")
+.mouseenter(function(){
+    beep4.play();
+});
+
+var beep5= $("#liechtensteinaudio")[0];
+$("#Liechtenstein")
+.mouseenter(function(){
+    beep5.play();
+});
+
+var beep6= $("#dutchaudio")[0];
+$("#Netherlands")
+.mouseenter(function(){
+    beep6.play();
+});
+
+var beep7= $("#belgium1audio")[0];
+$("#Belgium1")
+.mouseenter(function(){
+    beep7.play();
+});
+
+var beep8= $("#belgium2audio")[0];
+$("#Belgium2")
+.mouseenter(function(){
+    beep8.play();
+});
+
+var beep9= $("#germanyaudio")[0];
+$("#Germany")
+.mouseenter(function(){
+    beep9.play();
+});
+
+var beep10= $("#denmarkaudio")[0];
+$("#Denmark")
+.mouseenter(function(){
+    beep10.play();
+});
+
+var beep11= $("#ukaudio")[0];
+$("#UK")
+.mouseenter(function(){
+    beep11.play();
+});
+
+var beep12= $("#irelandaudio")[0];
+$("#Ireland")
+.mouseenter(function(){
+    beep12.play();
+});
+
+var beep13= $("#luxaudio")[0];
+$("#Luxembourg")
+.mouseenter(function(){
+    beep13.play();
+});
+
+var beep14= $("#norwayaudio")[0];
+$("#Norway")
+.mouseenter(function(){
+    beep14.play();
+});
+
+var beep15= $("#swedenaudio")[0];
+$("#Sweden")
+.mouseenter(function(){
+    beep15.play();
+});
+
+var beep16= $("#finlandaudio")[0];
+$("#Finland")
+.mouseenter(function(){
+    beep16.play();
+});
+
+var beep17= $("#icelandaudio")[0];
+$("#Iceland")
+.mouseenter(function(){
+    beep17.play();
+});
+
+var beep18= $("#andorraaudio")[0];
+$("#Andorra")
+.mouseenter(function(){
+    beep18.play();
+});
+
+var beep19= $("#italyaudio")[0];
+$("#Italy")
+.mouseenter(function(){
+    beep19.play();
+});
+
+var beep20= $("#czechaudio")[0];
+$("#Czech")
+.mouseenter(function(){
+    beep20.play();
+});
+
+var beep21= $("#austriaaudio")[0];
+$("#Austria")
+.mouseenter(function(){
+    beep21.play();
+});
+
+var beep22= $("#slovaudio")[0];
+$("#Slovenia")
+.mouseenter(function(){
+    beep22.play();
+});
+
+var beep23= $("#croatiaaudio")[0];
+$("#Croatia")
+.mouseenter(function(){
+    beep23.play();
+});
+
+var beep24= $("#BHaudio")[0];
+$("#BH")
+.mouseenter(function(){
+    beep24.play();
+});
+
+var beep25= $("#serbiaaudio")[0];
+$("#Serbia")
+.mouseenter(function(){
+    beep25.play();
+});
+
+var beep26= $("#monteaudio")[0];
+$("#Montenegro")
+.mouseenter(function(){
+    beep26.play();
+});
+
+var beep27= $("#maceaudio")[0];
+$("#Macedonia")
+.mouseenter(function(){
+    beep27.play();
+});
+
+var beep28= $("#slovakiaaudio")[0];
+$("#Slovakia")
+.mouseenter(function(){
+    beep28.play();
+});
+
+var beep29= $("#hunaudio")[0];
+$("#Hungary")
+.mouseenter(function(){
+    beep29.play();
+});
+
+var beep30= $("#alaudio")[0];
+$("#Albania")
+.mouseenter(function(){
+    beep30.play();
+});
+
+var beep31= $("#kosaudio")[0];
+$("#Kosovo")
+.mouseenter(function(){
+    beep31.play();
+});
+
+var beep32= $("#greeceaudio")[0];
+$("#Greece")
+.mouseenter(function(){
+    beep32.play();
+});
+
+var beep33= $("#turkeyaudio")[0];
+$("#Turkey")
+.mouseenter(function(){
+    beep33.play();
+});
+
+var beep34= $("#bulaudio")[0];
+$("#Bulgaria")
+.mouseenter(function(){
+    beep34.play();
+});
+
+var beep35= $("#romaudio")[0];
+$("#Romania")
+.mouseenter(function(){
+    beep35.play();
+});
+
+var beep36= $("#polaudio")[0];
+$("#Poland")
+.mouseenter(function(){
+    beep36.play();
+});
+
+var beep37= $("#molaudio")[0];
+$("#Moldova")
+.mouseenter(function(){
+    beep37.play();
+});
+
+var beep38= $("#ukraineaudio")[0];
+$("#Ukraine")
+.mouseenter(function(){
+    beep38.play();
+});
+
+var beep39= $("#belaudio")[0];
+$("#Belarus")
+.mouseenter(function(){
+    beep39.play();
+});
+
+var beep40= $("#latviaaudio")[0];
+$("#Latvia")
+.mouseenter(function(){
+    beep40.play();
+});
+
+var beep41= $("#litaudio")[0];
+$("#Lithuania")
+.mouseenter(function(){
+    beep41.play();
+});
+
+var beep42= $("#estoniaaudio")[0];
+$("#Estonia")
+.mouseenter(function(){
+    beep42.play();
+});
+
+var beep43= $("#rusaudio")[0];
+$("#Russia")
+.mouseenter(function(){
+    beep43.play();
+});
+
+var beep14= $("#uk1audio")[0];
+$("#UK1")
+.mouseenter(function(){
+    beep14.play();
+});
